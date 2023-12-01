43ab82192393752f39ef006eba1c5f313eedd220
HoanNguyen <hoannguyen13520@gmail.com>
2023-12-01 11:42:08 +0700

    push app

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { LogBox } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Toast from "react-native-toast-message";
+import "react-native-gesture-handler";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./Redux/store";
+
+// Context API
+import Auth from "./Context/store/Auth";
+
+// Navigatiors
+import Main from "./Navigators/Main";
+
+// Screens
+import Header from "./Shared/Header";
+import { NativeBaseProvider } from "native-base";
+
+LogBox.ignoreAllLogs(true);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Auth>
+      <Provider store={store}>
+        <NavigationContainer>
+          <NativeBaseProvider>
+            <Header />
+            <Main />
+            <Toast ref={(ref) => Toast.setRef(ref)} />
+          </NativeBaseProvider>
+        </NavigationContainer>
+      </Provider>
+    </Auth>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
